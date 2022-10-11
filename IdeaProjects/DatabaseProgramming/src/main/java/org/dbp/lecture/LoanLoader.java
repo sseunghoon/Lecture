bc3ceca29191da1cd23f4fa4feaa7bca2d236ef9
sseunghoon <sseunghun99@naver.com>
2022-10-11 23:45:54 +0900

    no update?

--- IdeaProjects/DatabaseProgramming/src/main/java/org/dbp/lecture/LoanLoader.java
+++ IdeaProjects/DatabaseProgramming/src/main/java/org/dbp/lecture/LoanLoader.java
@@ -2,9 +2,7 @@ package org.dbp.lecture;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 
 class Loan {
@@ -52,23 +50,55 @@ public class LoanLoader {
         Connection conn = DriverManager.getConnection("jdbc:mariadb://localhost:3306", id, pw);
         Statement stmt = conn.createStatement();
 
-        r.lines().forEach(str -> {
-            String arr[] = str.split(",");
-            list.add(new Loan(arr[0], arr[1], Integer.parseInt(arr[2])));
-        });
-
-        stmt.executeUpdate("CREATE OR REPLACE DATABASE midterm;");
-        stmt.executeUpdate("USE midterm;");
-        stmt.executeUpdate(
-                "CREATE TABLE loan (loan_number VARCHAR(50), " +
-                        "branch_name VARCHAR(50), amount INT);");
-
-        for (Loan loan: list) {
-            String insertInto = "INSERT INTO loan VALUES ('" + loan.getLoanNumber() + "','" + loan.getBranchName() + "'," + loan.getAmount() + ");";
-            System.out.println(insertInto);
-            stmt.executeUpdate(insertInto);
+        stmt.executeQuery("CREATE DATABASE IF NOT EXISTS dbp;");
+        stmt.executeQuery("USE dbp");
+        stmt.executeQuery("CREATE TABLE IF NOT EXISTS student(" +
+                "name VARCHAR(50)," +
+                "id VARCHAR(50)," +
+                "point INT);");
+        ResultSet rs = stmt.executeQuery("DESCRIBE student;");
+        ResultSetMetaData rsmd = rs.getMetaData();
+        for (int i = 1; i < rsmd.getColumnCount(); i++) {
+            System.out.print(rsmd.getColumnName(i) + "(" + rsmd.getColumnLabel(i) + ") ");
         }
-        r.close();
+        System.out.println();
+        while(rs.next()) {
+            for (int i = 1; i <= rsmd.getColumnCount() ; i++) {
+                System.out.print(rs.getString(i) + " ");
+            }
+            System.out.println();
+        }
 
+
+//        stmt.executeUpdate("USE assignment1;");
+//        ResultSet rs = stmt.executeQuery("SELECT * FROM charger;");
+//        ResultSetMetaData rsmd = rs.getMetaData();
+//        System.out.println(rsmd.getColumnName(1) + ":" + rsmd.getColumnTypeName(1));
+//        while(rs.next()) {
+//            for (int i = 1; i <= rsmd.getColumnCount(); i++) {
+//                System.out.println(rs.getString(i));
+//            }
+//        }
+
+//
+//        r.lines().forEach(str -> {
+//            String arr[] = str.split(",");
+//            list.add(new Loan(arr[0], arr[1], Integer.parseInt(arr[2])));
+//        });
+//
+//        stmt.executeUpdate("CREATE OR REPLACE DATABASE midterm;");
+//        stmt.executeUpdate("USE midterm;");
+//        stmt.executeUpdate(
+//                "CREATE TABLE loan (loan_number VARCHAR(50), " +
+//                        "branch_name VARCHAR(50), amount INT);");
+//
+//        for (Loan loan: list) {
+//            String insertInto = "INSERT INTO loan VALUES ('" + loan.getLoanNumber() + "','" + loan.getBranchName() + "'," + loan.getAmount() + ");";
+//            System.out.println(insertInto);
+//            stmt.executeUpdate(insertInto);
+//        }
+
+        r.close();
+        conn.close();
     }
 }
---
+++ IdeaProjects/DatabaseProgramming/src/main/java/org/dbp/lecture/UserDefinedFunc.java
@@ -0,0 +1,31 @@
+package org.dbp.lecture;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class UserDefinedFunc {
+
+    public static void main(String[] args) throws Exception{
+        String id = "root";
+        String pw = "1234";
+        Connection conn = DriverManager.getConnection("jdbc:mariadb://localhost:3306", id, pw);
+        Statement stmt = conn.createStatement();
+
+        stmt.executeQuery("use dbp");
+        System.out.println("Connecting to...:" + conn.getCatalog());
+        stmt.executeUpdate("CREATE OR REPLACE FUNCTION SUM_VALUES() RETURNS INT " +
+                "BEGIN DECLARE sum INT;" +
+                "SET sum = 0;" +
+                "FOR i IN (SELECT x FROM ten)" +
+                "DO " +
+                "SET sum = sum + i.x;" +
+                "END FOR;" +
+                "RETURN sum;" +
+                "END");
+        ResultSet rs = stmt.executeQuery("SELECT SUM_VALUES();");
+        rs.next();
+        System.out.println(rs.getInt(1));
+    }
+}
